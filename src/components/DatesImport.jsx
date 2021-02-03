f14e9caecccdd38c3e4ab54a2c19abc86a093e88
collie-moore <moorecollins6@gmail.com>
2021-02-03 12:03:14 +0300

    Folders update

---
+++ src/components/DatesImport.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+export default function Sec1() {
+  return (
+    <div className="sec1">
+      <div className="first-sec">
+        <span>Flower Exports</span>
+        <h4>
+          I need text to explain more about who you are.
+        </h4>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi
+          recusandae, ducimus vel rerum accusamus odit provident nobis ratione
+          quisquam obcaecati atque fuga maiores! Tenetur aspernatur alias unde
+          facilis eveniet? Eius! Lorem ipsum dolor sit amet, Fugit ipsam nostrum
+          minus alias, expedita.
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi
+          recusandae, ducimus vel rerum accusamus odit provident nobis ratione
+          quisquam obcaecati atque fuga maiores! Tenetur aspernatur alias unde
+          facilis eveniet? Eius! Lorem ipsum dolor sit amet, Fugit ipsam nostrum
+          minus alias, expedita.
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi
+          recusandae, ducimus vel rerum accusamus odit provident nobis ratione
+          quisquam obcaecati atque fuga maiores! Tenetur aspernatur alias unde
+          facilis eveniet? Eius! Lorem ipsum dolor sit amet, Fugit ipsam nostrum
+          minus alias, expedita.
+        </p>
+        <h6>
+          <i className="fas fa-user"></i> David Karare
+        </h6>
+      </div>
+      <div className="img-sec">
+        <img
+          src="#https://demo.w3layouts.com/demos_new/template_demo/29-02-2020/captivate-liberty-demo_Free/232105078/web/assets/images/about1.png"
+          alt="Nice Flower pic"
+        />
+      </div>
+    </div>
+  );
+}
